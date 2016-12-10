3c4392cdb910ee994011d60c2fffb484532f2af2
Benny <hoaben13@student.westerdals.no>
2016-12-10 19:39:05 +0100

    added api folder

---
+++ SensorApi/SensorApi/Controllers/SensorController.cs
@@ -0,0 +1,25 @@
+﻿using SensorApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SensorApi.Controllers
+{
+    public class SensorController : ApiController
+    {
+        public IEnumerable<sensorDataTable> GetSensorDatas()
+        {
+            using (SensorOrmDataContext sensorOrm = new SensorOrmDataContext())
+            {
+                List<sensorDataTable> sensorList = (from sensorDataTable in sensorOrm.sensorDataTables
+                                               select sensorDataTable).ToList();
+                var count = sensorList.Count;
+
+                return sensorList.Skip(count - 10);
+            }   
+        }
+    }
+}
